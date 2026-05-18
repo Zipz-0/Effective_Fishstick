60a83f45f81d2b8c868cb09360cdb5b628c55efe
CESAR\cesar <cesarrobles163@yahoo.com>
2026-05-18 15:32:57 -0400

    A* grid

---
+++ Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    Grid grid;
+    void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+
+   void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(startNode);
+
+        while(openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+            for(int i = 1; i < openSet.Count; i++)
+            {
+                if(openSet[i].fCost <currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
+                currentNode = openSet[i];
+            }
+
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if(currentNode == targetNode)
+                return;
+        }
+    }
+}
